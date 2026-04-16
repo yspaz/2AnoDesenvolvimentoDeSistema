c77abd96a846182d1a124826cf80def18f64543b
yspaz <yasminpaz438@gmail.com>
2026-04-16 17:29:57 -0300

    aula 1 - javacript
    atividades

---
+++ senai/javascript/aula 1 - atividade/atividade 2/scripts/atv2.js
@@ -0,0 +1,4 @@
+var numero = parseInt(prompt("Digite um número: "))
+var anterior = numero - 1
+var sucessor = numero + 1
+alert("O número anterior é: " + anterior + "\nO número digitado é: " + numero + "\nO número sucessor é: " + sucessor)
