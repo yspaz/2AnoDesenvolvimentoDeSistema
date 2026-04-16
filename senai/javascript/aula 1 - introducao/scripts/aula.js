fb1956818846552f49b8a51f4570221cfd4536df
yspaz <yasminpaz438@gmail.com>
2026-04-16 15:51:37 -0300

    aula 1 js
    variaveis. console e hierarquia

--- senai/javascript/aula 1 - introducao/scripts/aula.js
+++ senai/javascript/aula 1 - introducao/scripts/aula.js
@@ -1,4 +1,29 @@
+// resposta no console
 console.log("olá mundo!");
-alert("olá mundo!");
-prompt("qual seu nome?");
-confirm("você é estudante?");
+
+// caixas de dialogo
+// alert("olá mundo!");
+// prompt("qual seu nome?");
+// confirm("você é estudante?");
+
+// acessa documento,busca o elemento com id selecionado
+// inner = dentro
+document.getElementById("resposta").innerText = "<h1>linda tarde!</h1>"
+document.getElementById("resposta").innerHTML = "<h1>linda tarde!</h1>"
+
+// string - caractere ("Yasmin")
+var nome = "Yasmin";
+//  boolean - lógico (true ou false)
+var ligado = true;
+// number - numérico (16)
+// *obs para numeros quebrados - float()
+// *obs para numeros inteiros - Int()
+var idade = 16;
+var dinheiro = 100.50;
+// array - lista , vetor
+var listaCompras = []
+console.log(typeof(nome)); 
+console.log(typeof(ligado)); 
+console.log(typeof(idade)); 
+console.log(typeof(dinheiro));
+console.log(typeof(listaCompras));
---
+++ senai/javascript/aula 1 - introducao/scripts/usuário.js
@@ -0,0 +1,4 @@
+var nome = prompt("qual seu nome?");
+
+// document.getElementById("resposta").innerHTML = "<h1> " + nome + "</h1>";
+document.getElementById("resposta").innerHTML = `<h1> Olá seja bem vindo ${nome} </h1>`;
