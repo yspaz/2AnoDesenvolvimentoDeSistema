bcd1b94bde3c206ccf14a7c20879cbe50c01dc90
FilipeFarias08 <yasminpaz438@gmail.com>
2026-06-11 13:48:34 -0300

    aula 7 - atividade

---
+++ senai/javascript/aula 7 - atividade/script/script.js
@@ -0,0 +1,14 @@
+const nome = "Mariano"
+const idade = 16
+const online = true
+const partidas = [2,3,4]
+
+let jogoFavorito = {
+    nome:"Counter-Strike 2.0",
+    anoLancamento: 2023
+};
+
+console.log(online, typeof online)
+console.log(idade, typeof idade)
+console.log(nome, typeof nome)
+console.log(partidas, typeof partidas)
